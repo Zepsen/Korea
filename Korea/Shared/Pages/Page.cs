f7ae2ba234dc1ad11f2be9a739cc1eecd2e540d0
Dmytro Zepsen <Dmytro.Zepsen@thomsonreuters.com>
2019-09-19 14:15:56 +0300

    up

--- Korea/Shared/Pages/Page.cs
+++ Korea/Shared/Pages/Page.cs
@@ -10,13 +10,8 @@ namespace Korea.Shared.Pages
 		[CascadingParameter]
 		protected IBasePage basePage { get; set; }
 
-		protected virtual bool HasHeader { get; set; } = true;
-		protected virtual bool HasFooter { get; set; } = true;
-		protected virtual bool HasBreadcrumbs { get; set; } = true;
-	
 		protected override void OnInitialized()
 		{			
-			basePage.HasHeader = HasHeader;
 			Breadcrumbs();
 		}
 
