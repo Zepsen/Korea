94fd8f5c8e4fc5e27c9096e264c22f7dab3ecd89
Dmytro Zepsen <Dmytro.Zepsen@thomsonreuters.com>
2019-09-19 14:58:25 +0300

    add activegood

--- Korea/Shared/Models/Good.cs
+++ Korea/Shared/Models/Good.cs
@@ -3,6 +3,7 @@ namespace Korea.Shared.Models
 {
 	public class Good
 	{
+		public int Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public string Image { get; set; }
